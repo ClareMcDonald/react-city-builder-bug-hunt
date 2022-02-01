ec495c5f645f2d9ebbbed148438989f3e4c1dcf2
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-01 14:57:05 -0800

    debugged:

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SloganForm from './SloganForm';
 import CityNameInput from './CityNameInput';
 import City from './City';
 
-function App() {
+export default function App() {
   const [skylineId, setSkyline] = useState(1);
   const [waterfrontId, setWaterfront] = useState(1);
   const [castleId, setCastle] = useState(1);
@@ -16,22 +16,23 @@ function App() {
   const [slogans, setSlogans] = useState(['The City of Excellence']);
 
   return (
-    <h3>My App</h3>
-    <div className="App">
-      <City skylineId={skylineId} castleId={castleId} />
-      <h1>Welcome to beautiful {cityName}</h1>
-      <div className='bottom'>
-        <CityNameInput setCityName={props.setCityName} />
-        <section className='dropdowns'>
-          <WaterfrontDropdown />
-          <SkylineDropdown setSkyline={setSkyline}/>
-          <CastleDropdown setCastle={setCastle} />
-        </section>
-        <SloganForm setSlogans={setSlogans} slogans={slogans} />
-        <SloganList/>
+    <>
+      <h3>My App</h3>
+      <div className="App">
+        <City skylineId={skylineId} castleId={castleId} waterfrontId={waterfrontId}/>
+        <h1>Welcome to beautiful {cityName}</h1>
+        <div className='bottom'>
+          <CityNameInput setCityName={setCityName} />
+          <section className='dropdowns'>
+            <WaterfrontDropdown setWaterfront={setWaterfront}/>
+            <SkylineDropdown setSkyline={setSkyline}/>
+            <CastleDropdown setCastle={setCastle} />
+          </section>
+          <SloganForm setSlogans={setSlogans} slogans={slogans} />
+          <SloganList slogans={slogans}/>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
-export App;
